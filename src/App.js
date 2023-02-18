2b91949b08a056c2c0dab33cfcec1e16398a1cdd
Apopheniaeu <53937282+Apopheniaeu@users.noreply.github.com>
2023-02-18 15:38:15 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./styles.css";
+import Logo from "./images/Apophenia_LogoA_White.png";
+
+export default function App() {
+  return (
+    <>
+      <div class="vimeo-wrapper">
+        <iframe
+          title="Microbial Menage"
+          src="https://player.vimeo.com/video/763046666?h=74ea60f1d7&amp;badge=0&amp;background=1&autoplay=1&loop=1&byline=0&title=0"
+          frameBorder="0"
+          webkitallowfullscreen
+          mozallowfullscreen
+          allowFullScreen
+        ></iframe>
+      </div>
+
+      <div class="logo">
+        <img src={Logo} className="logo" alt="ALogo" />
+      </div>
+    </>
+  );
+}
