4148c98aeed345d624c15e038d85f481b97c842f
apopheniaeu <info@apophenia.eu>
2023-02-20 05:35:43 +0000

    Update 3 files

--- src/App.js
+++ src/App.js
@@ -3,21 +3,10 @@ import "./styles.css";
 import Logo from "./images/Apophenia_LogoA_White.png";
 
 export default function App() {
-  useEffect(() => {
-    const iframe = document.getElementById("my-video");
-    const player = new window.Vimeo.Player(iframe);
-
-    player.on("play", () => {
-      const variableLogo = document.querySelector(".variable-logo");
-      variableLogo.style.animationPlayState = "running";
-    });
-  }, []);
-
   return (
     <>
       <div class="vimeo-wrapper">
         <iframe
-          id="my-video"
           title="Microbial Menage"
           src="https://player.vimeo.com/video/763046666?h=74ea60f1d7&amp;badge=0&amp;background=1&autoplay=1&loop=1&byline=0&title=0"
           frameBorder="0"
