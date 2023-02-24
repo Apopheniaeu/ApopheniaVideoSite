9055354bc061326cc578f19ef2023ced1dd6fb42
apopheniaeu <info@apophenia.eu>
2023-02-24 05:40:47 +0000

    Update App.js and styles.css

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ export default function App() {
       <div className="vimeo-wrapper">
         <iframe
           title="Microbial Menage"
-          src="https://player.vimeo.com/video/801803036?h=74ea60f1d7&amp;badge=0&amp;background=1&autoplay=1&loop=1&byline=1&title=0"
+          src="https://player.vimeo.com/video/801803036?h=74ea60f1d7&amp;badge=0&amp;background=1&autoplay=1&loop=1&byline=1&title=0&muted=0"
           frameBorder="0"
           webkitallowfullscreen
           mozallowfullscreen
